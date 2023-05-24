16bf4ae69e30689ecd9e75c97c32bb14f4bcbed6
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-25 03:48:16 +0600

    completed

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const users = [
   { id: 4, name: "ammu", email: "ammu@gmail" },
 ];
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("server running");
@@ -18,6 +20,15 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log("post api hitting");
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
